6c9dc0f263f93f4ea621d662257d33c9f0e3eb67
4ions <leovalenciasantana@gmail.com>
2021-05-15 13:15:50 -0500

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -10,4 +10,12 @@ $(function () {
             }
             $('div.amenities h4').html(Object.values(listAmenities).join(', ') || '&nbsp;');
         });
+
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+        if (data.status === 'OK') {
+            $('DIV#api_status').addClass('available');
+        } else {
+            $('DIV#api_status').removeClass('available');
+        }
+    });
 });
